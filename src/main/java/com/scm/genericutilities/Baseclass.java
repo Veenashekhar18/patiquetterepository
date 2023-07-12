6593931ee884f80dc972aacab2026575903eff5c
Veenashekhar18 <veenaraichur58@gmail.com>
2023-07-12 17:00:04 +0530

    commit 3

--- src/main/java/com/scm/genericutilities/Baseclass.java
+++ src/main/java/com/scm/genericutilities/Baseclass.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 
 import com.scm.pomobjectrepository.Loginpage;
 
@@ -60,7 +61,7 @@ public class Baseclass {
 			String title=driver.getTitle();
 			System.out.println(title);
 	}
-	@BeforeClass
+	@BeforeMethod
 	public void logintoapp() throws Throwable {
 		
 		//login as admin
